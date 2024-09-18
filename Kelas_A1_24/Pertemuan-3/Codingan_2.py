f5c1a23873bc1767f89f7f2e2de3da40a178a7df
Rusdiansyah <rsdnsyhh@gmail.com>
2024-09-18 20:40:27 +0800

    Add files via upload

---
+++ Kelas_A1_24/Pertemuan-3/Codingan_2.py
@@ -0,0 +1,9 @@
+a = input("Masukkan angka 1 : ")
+b = input("Masukkan angka 2 : ")
+
+if a>b:
+    print ("Kelinci")
+elif a<b:
+    print ("Kura-Kura")
+else:
+    print ("Sapi")
---
+++ Kelas_A1_24/Pertemuan-3/codingan_1.py
@@ -0,0 +1,6 @@
+cuaca = input("Apakah cuaca hari ini cerah? [ya/tidak]: ")
+
+if cuaca == "ya":
+        print("Kamu bisa berangkat")
+if cuaca == "tidak":
+        print("Kamu tetap di rumah")
