2fcd3ae6f1b3e0bec43d1874c0ddf6932ccb9391
2409106013_Rusdiansyah <rsdnsyhh@gmail.com>
2024-09-25 09:11:14 +0800

    Add files via upload

---
+++ Kelas_A1_24/Pertemuan-4/codingan_pertemuan_4.py
@@ -0,0 +1,56 @@
+# ulang = 10
+# for i in range(ulang):
+# print("Perulangan ke-" + str(i))
+
+# simpan = [12, "udin petot",14.5, True, 'A']
+# for i in simpan:
+#     print(i,end= ' ')
+
+# for i in range(1, 10, 2): #(awal, akhir lompatan)
+#     print(i)
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+# print()
+
+# jawab = 'ya'
+# hitung = 0
+
+# while(jawab == 'ya'): #kondisi/syarat
+#     hitung += 1 #hitung = hitung + 2
+#     jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# hitung = 0
+# while True: #selalu benar
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#     break
+
+# print(f"Total Perulangan: {hitung}")
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+# print(i)
+
+#studi kasus 1
+
+# bilangan = 0
+# while True:
+#     angka = int(input("Masukkan Angka = "))
+#     if angka < 0:
+#         break
+#     bilangan += angka
+    
+# print("Total bilangan: " + str(bilangan))
+
+#studi kasus 2
+
+# for i in range(1 , 20, 3):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
